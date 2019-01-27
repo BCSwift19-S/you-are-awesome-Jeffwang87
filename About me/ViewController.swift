e8f068c556dc2e9397dfdafa2b58f4d4fce27be6
wxt <wxt@wxtdembp.bc.edu>
2019-01-27 12:11:48 -0500

    2.14

--- About me/ViewController.swift
+++ About me/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
 
     @IBAction func MessagebuttonPressed(_ sender: UIButton) {
         let messages = ["You AreFantastic!","You Are Great!","You Are Amazing!","You Are Awesome!"]
-        var newIndex = -1
+        var newIndex: Int
         repeat{
             newIndex = Int.random(in: 0..<messages.count)
     } while index == newIndex
