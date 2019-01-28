a81a7c086aa6c3ac7549715065e9c2754b8f30a9
wxt <wxt@wxtdembp.bc.edu>
2019-01-27 22:47:53 -0500

    2.16

--- About me/ViewController.swift
+++ About me/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     var index = 0
+    var imageindex = -1
 
+    @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var Messagelabel: UILabel!
     
     override func viewDidLoad() {
@@ -22,11 +24,20 @@ class ViewController: UIViewController {
     @IBAction func MessagebuttonPressed(_ sender: UIButton) {
         let messages = ["You AreFantastic!","You Are Great!","You Are Amazing!","You Are Awesome!"]
         var newIndex: Int
+    
         repeat{
             newIndex = Int.random(in: 0..<messages.count)
     } while index == newIndex
         index = newIndex
         Messagelabel.text = messages[index]
+        
+        repeat{
+            newIndex = Int.random(in: 0..<4)
+        }while imageindex == newIndex
+        
+        imageindex = newIndex
+        awesomeImageView.image = UIImage(named: "image\(imageindex)")
+    
 }
 
 }
